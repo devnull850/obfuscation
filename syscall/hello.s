29a76f73f7edc83c03e35bec7b132501af633d1e
devnull850 <dev.null850@gmail.com>
2021-11-12 15:20:54 -0500

    Leverage stack operation push -dev0

--- syscall/hello.s
+++ syscall/hello.s
@@ -1,12 +1,12 @@
 	.section .text
+	movq	$0xa21646c726f,%rax
+	push	%rax
 	movq	$0xf7eb050f0000003c,%rax
 	movq	$0xa0cb29606c6c6574,%rcx
 	xorq	%rcx,%rax
-	movq	%rax,0xfffffffffffffff0(%rsp)
-	movq	$0xa21646c726f,%rax
-	movq	%rax,0xfffffffffffffff8(%rsp)
+	push	%rax
 	movl	$1,%edi
-	leaq	0xfffffffffffffff0(%rsp),%rsi
+	leaq	(%rsp),%rsi
 	movl	$0xe,%edx
 	movl	$1,%eax
-	jmp	.text+6
+	jmp	.text+0x11
